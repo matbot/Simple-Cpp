5eaa7c85dacb9f1981e94e8a46af40c016f4d9a9
matbotx <mathewmcdade@gmail.com>
2018-12-06 15:26:26 -0800

    updated box

--- box/BoxMain.cpp
+++ box/BoxMain.cpp
@@ -9,19 +9,20 @@ using namespace std;
 int main()
 {
 	double height, length, width;
-	cout << "Give, me, them, digits!" << endl;
+	cout << "Descibe the box and I'll give you the volume and surface area." << endl;
+	cout << "What are the boxes height, length, and width?" << endl;
 	cin >> height >> length >> width;
 	
-	Box box1;
+//	Box box1;
 	Box box2(height, length, width);
 
-	double volume1 = box1.getVolume();
-	double surface1 = box1.getSurfaceArea();
+//	double volume1 = box1.getVolume();
+//	double surface1 = box1.getSurfaceArea();
 
 	double volume2 = box2.getVolume();
 	double surface2 = box2.getSurfaceArea();
 
-	cout << volume1 << endl << surface1 << endl << volume2 << endl << surface2 << endl;
+	cout << volume2 << endl << surface2 << endl;
 
 return 0;
 }
